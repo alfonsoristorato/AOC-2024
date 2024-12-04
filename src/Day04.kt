9d9b60abe1e78c2df36a3e4d5581d772377407ef
alfonsoristorato <alfonsoristorato@gmail.com>
2024-12-04 11:50:45 +0000

    day 4 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,14 +1,21 @@
 fun main() {
     val input = readInput("Day04")
     var xmasCounter = 0
+    var masXCounter = 0
     input.forEachIndexed { lineIndex, line ->
         val xIndices = line.indices.filter { line[it] == 'X' }
+        val aIndices = line.indices.filter { line[it] == 'A' }
         xIndices.forEach { xCharIndex ->
             xmasCounter += findXmas(input, xCharIndex, lineIndex)
+        }
+        aIndices.forEach { aCharIndex ->
+            masXCounter += findMasXShaped(input, aCharIndex, lineIndex)
         }
     }
     print("PART 1: ")
     xmasCounter.println()
+    print("PART 2: ")
+    masXCounter.println()
 }
 
 fun findXmas(input: List<String>, xCharIndex: Int, lineIndex: Int): Int {
@@ -17,6 +24,26 @@ fun findXmas(input: List<String>, xCharIndex: Int, lineIndex: Int): Int {
         checkWithDirections(input, xCharIndex, lineIndex, "MAS", direction).takeIf { it }?.let {
             counter++
         }
+    }
+    return counter
+}
+
+fun findMasXShaped(input: List<String>, aCharIndex: Int, lineIndex: Int): Int {
+    var counter = 0
+    runCatching {
+        val topLeft = input[lineIndex - 1][aCharIndex - 1]
+        val bottomRight = input[lineIndex + 1][aCharIndex + 1]
+
+        val topRight = input[lineIndex - 1][aCharIndex + 1]
+        val bottomLeft = input[lineIndex + 1][aCharIndex - 1]
+
+        if (topLeft == 'X' || bottomRight == 'X' || topRight == 'X' || bottomLeft == 'X' || topLeft == 'A' || bottomRight == 'A' || topRight == 'A' || bottomLeft == 'A') {
+            return@runCatching
+        }
+        if (topLeft == bottomRight || topRight == bottomLeft) {
+            return@runCatching
+        }
+        counter++
     }
     return counter
 }
