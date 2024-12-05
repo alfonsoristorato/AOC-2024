4286fed996e3e5972ff3fa42f7ee537c75629e54
alfonsoristorato <alfonsoristorato@gmail.com>
2024-12-05 11:28:32 +0000

    day 5 part 2 - even more ugly

--- src/Day05.kt
+++ src/Day05.kt
@@ -2,16 +2,16 @@ fun main() {
     val input = readInput("Day05")
 
     val rules = mutableListOf<Pair<Int, Int>>()
-    val updates = mutableListOf<List<Int>>()
-    val validUpdates = mutableListOf<List<Int>>()
+    val updates = mutableListOf<MutableList<Int>>()
+    val validUpdates = mutableListOf<MutableList<Int>>()
+    val invalidUpdates = mutableListOf<MutableList<Int>>()
     input.forEach { line ->
         runCatching {
             val (left, right) = line.split("|")
             rules.add(Pair(left.toInt(), right.toInt()))
         }
         runCatching {
-            updates.add(line.split(",").map { it.toInt() })
-
+            updates.add(line.split(",").map { it.toInt() }.toMutableList())
         }
     }
     updates.forEach { update ->
@@ -23,9 +23,38 @@ fun main() {
                 }
             }
         }
-        validUpdate && validUpdates.add(update)
+        validUpdate && validUpdates.add(update) || invalidUpdates.add(update)
     }
     print("PART 1: ")
-    println(validUpdates.sumOf { it[(it.size-1) / 2] }
-    )
+    println(validUpdates.sumOf { it[(it.size - 1) / 2] })
+
+    var invalidUpdatesCounter = invalidUpdates.size
+    while (invalidUpdatesCounter > -1) {
+        invalidUpdates.forEach { update ->
+            var validUpdate = true
+            rules.forEach {
+                if (update.contains(it.first) && update.contains(it.second)) {
+                    if (update.indexOf(it.first) > update.indexOf(it.second)) {
+                        val moveToRightIndex = update.indexOf(it.second)
+                        val moveToRightValue = update[moveToRightIndex]
+
+                        update.removeAt(moveToRightIndex)
+                        update.addLast(moveToRightValue)
+                    }
+                }
+            }
+            rules.forEach {
+                if (update.contains(it.first) && update.contains(it.second)) {
+                    if (update.indexOf(it.first) > update.indexOf(it.second)) {
+                        validUpdate = false
+                    }
+                }
+            }
+            if (validUpdate) invalidUpdatesCounter-- else invalidUpdatesCounter++
+        }
+    }
+    print("PART 2: ")
+    println(invalidUpdates.sumOf { it[(it.size - 1) / 2] })
 }
+
+
